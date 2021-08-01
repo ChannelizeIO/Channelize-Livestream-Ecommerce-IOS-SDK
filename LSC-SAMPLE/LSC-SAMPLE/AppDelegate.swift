189a3f0f81d8df58c8af28594a08aac2a6235f42
rohitphogat19 <rohit.kumar@bigsteptech.com>
2021-08-01 22:47:47 +0530

    Added Sample Project Code

--- LSC-SAMPLE/LSC-SAMPLE/AppDelegate.swift
+++ LSC-SAMPLE/LSC-SAMPLE/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import LiveBroadcastSDK
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        var configurations = LBConfig()
+        configurations.apiPublicKey = KeyCenter.publicKey
+        LiveBroadcast.configure(with: configurations)
         return true
     }
 
---
+++ LSC-SAMPLE/LSC-SAMPLE/KeyCenter.swift
@@ -0,0 +1,12 @@
+//
+//  KeyCenter.swift
+//  LSC-SAMPLE
+//
+//  Created by BigStep on 01/08/21.
+//
+
+import Foundation
+
+class KeyCenter {
+    class var publicKey = "SET_YOUR_PUBLIC_KEY"
+}
