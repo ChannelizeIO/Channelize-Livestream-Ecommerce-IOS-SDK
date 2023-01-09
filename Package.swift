4886354ab4493c821135e7e4eb1af48c318723b0
Rohit <rohit.kumar@bigsteptech.com>
2023-01-09 22:36:53 +0530

    Added Demo Wrapper

--- Package.swift
+++ Package.swift
@@ -1,4 +1,6 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
@@ -7,25 +9,37 @@ let package = Package(
         .iOS(.v12)
     ],
     products: [
-        .library(
-            name: "ChannelizeLSC",
-            targets: ["LiveBroadcastAPI", "LiveBroadcastUI"]),
+        .library( name: "ChannelizeLSC", targets: ["LiveBroadcastAPI", "LiveBroadcastUI", "Wrapper"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm.git", from: "2.29.1"),
-        .package(url: "https://github.com/StyleShare/HLSCachingReverseProxyServer.git", from: "0.1.0"),
-        .package(url: "https://github.com/AgoraIO/AgoraRtcEngine_iOS.git", from: "4.1.0"),
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.13.4")
+        .package(name: "AWSiOSSDKV2", url: "https://github.com/aws-amplify/aws-sdk-ios-spm.git", from: "2.29.1"),
+        .package(name: "AgoraRtcKit", url: "https://github.com/AgoraIO/AgoraRtcEngine_iOS.git", from: "4.1.0"),
+        .package(name: "SDWebImage", url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.13.4")
     ],
     targets: [
+        .target(
+            name: "Wrapper",
+            dependencies: [
+                "AgoraRtcKit",
+                "LiveBroadcastAPI",
+                "LiveBroadcastUI",
+                .product(name: "AWSMobileClient", package: "AWSiOSSDKV2"),
+                .product(name: "AWSIoT", package: "AWSiOSSDKV2"),
+                .product(name: "SDWebImage", package: "SDWebImage"),
+            ],
+            path: "Wrapper",
+            sources: ["Core"]
+        ),
         .binaryTarget(
             name: "LiveBroadcastAPI",
             url: "https://raw.githubusercontent.com/ChannelizeIO/Channelize-Livestream-Ecommerce-IOS-SDK/framework_source/LiveBroadcastAPI.xcframework.zip",
-            checksum: "5407b74f7919b3859dbe6028534ae6a8b2b6a33790a20455bb50460a124881d2"),
+            checksum: "a04ffe38920698c411cdff119e82240b90b1aae12f93e076f9dc1337103b66db"
+        ),
         .binaryTarget(
             name: "LiveBroadcastUI",
             url: "https://raw.githubusercontent.com/ChannelizeIO/Channelize-Livestream-Ecommerce-IOS-SDK/framework_source/LiveBroadcastUI.xcframework.zip",
-            checksum: "743bc40646b52568a329da7c8d243f1774f95470ff4e4bd89d4826cea5a1b495")
+            checksum: "6e4f93f0310357f05a0f0d03c594542ec730bd068641428119624151826b8141"
+        ),
     ]
 )
