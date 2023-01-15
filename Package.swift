800b1e93d2bdccddd2605769b3a32308fbc15663
Rohit <rohit.kumar@bigsteptech.com>
2023-01-15 19:54:27 +0530

    Added SDKs with Non Dependency Image Loading Support

--- Package.swift
+++ Package.swift
@@ -13,34 +13,74 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(name: "Kingfisher", url: "https://github.com/onevcat/Kingfisher.git", from: "7.5.0"),
-        .package(name: "AgoraRtcKit", url: "https://github.com/AgoraIO/AgoraRtcEngine_iOS.git", from: "4.1.0"),
         .package(name: "AWSiOSSDKV2", url: "https://github.com/aws-amplify/aws-sdk-ios-spm.git", from: "2.29.1"),
     ],
     targets: [
         .binaryTarget(
             name: "LiveBroadcastAPI",
-            url: "https://firebasestorage.googleapis.com/v0/b/chatapplication-bceea.appspot.com/o/LiveBroadcastAPI.xcframework.zip?alt=media&token=42c9676f-d20e-4dbd-82e0-4f8ab91dbb51",
-            checksum: "e72d1520d6e9286c54de90bb7810aae65c5f043ff74d275a9fa28f049667ca79"
+            url: "https://github.com/ChannelizeIO/Channelize-Livestream-Ecommerce-IOS-SDK/releases/download/v2.0/LiveBroadcastAPI.xcframework.zip",
+            checksum: "c582c239212a6212078b2f90d4b9803ad10547861328adf899a9060b2d2df31e"
         ),
         .binaryTarget(
             name: "LiveBroadcastUI",
-            url: "https://firebasestorage.googleapis.com/v0/b/chatapplication-bceea.appspot.com/o/LiveBroadcastUI.xcframework.zip?alt=media&token=8c531de1-6679-4f55-ae99-78bff0a62945",
-            checksum: "30ab720242bf722068874d40a2fce3f9e6e66f31621f2551c79ccbc269685570"
+            url: "https://github.com/ChannelizeIO/Channelize-Livestream-Ecommerce-IOS-SDK/releases/download/v2.0/LiveBroadcastUI.xcframework.zip",
+            checksum: "03a6f7d291ea25dcfd6c68a4b55fb246ee1bb422bb50b4084fcfc0315c80e5eb"
+        ),
+        .binaryTarget(
+            name: "AgoraAiEchoCancellationExtension",
+            url: "https://download.agora.io/swiftpm/AgoraRtcEngine_iOS/4.1.0/AgoraAiEchoCancellationExtension.xcframework.zip",
+            checksum: "a23ef7a266bbf0fb9bb45dfa47edf0c14db17a06a253d5b4e4775e5f70fe5f12"
+        ),
+        .binaryTarget(
+            name: "AgoraDav1d",
+            url: "https://download.agora.io/swiftpm/AgoraRtcEngine_iOS/4.1.0/AgoraDav1d.xcframework.zip",
+            checksum: "ed68179336ad2e1410e5a0bb4db00dcbd14d30601caa03d5c0086b895740094f"
+        ),
+         .binaryTarget(
+            name: "AgoraDrmLoaderExtension",
+            url: "https://download.agora.io/swiftpm/AgoraRtcEngine_iOS/4.1.0/AgoraDrmLoaderExtension.xcframework.zip",
+            checksum: "46256bc8d6b5d662bb1bb7f5ab88059d7076e42edce3af9c1f000dc4e74e5313"
+        ),
+        .binaryTarget(
+            name: "Agorafdkaac",
+            url: "https://download.agora.io/swiftpm/AgoraRtcEngine_iOS/4.1.0/Agorafdkaac.xcframework.zip",
+            checksum: "56dada9f7894557742cc153cf8b240397df6bbda29e6f8dcf325ed4f3ed650a4"
+        ),
+        .binaryTarget(
+            name: "Agoraffmpeg",
+            url: "https://download.agora.io/swiftpm/AgoraRtcEngine_iOS/4.1.0/Agoraffmpeg.xcframework.zip",
+            checksum: "aeb75bbe7a12370ce2e4774ae13bfe1d6cd19347039a9483820a0bdbe11088cf"
+        ),
+        .binaryTarget(
+            name: "AgoraRtcKit",
+            url: "https://download.agora.io/swiftpm/AgoraRtcEngine_iOS/4.1.0/AgoraRtcKit.xcframework.zip",
+            checksum: "71f13c7df5b7ff22e1272a3efbe0a7982c8d5a3a5d53e9c1a9093170ea0372cc"
+        ),
+        .binaryTarget(
+            name: "AgoraSoundTouch",
+            url: "https://download.agora.io/swiftpm/AgoraRtcEngine_iOS/4.1.0/AgoraSoundTouch.xcframework.zip",
+            checksum: "cdf7531aa768a8799134ed4f2762ffb25b25b4b17d285e6056398bf73c6d8fcf"
+        ),
+        .binaryTarget(
+            name: "AgoraVideoQualityAnalyzerExtension",
+            url: "https://download.agora.io/swiftpm/AgoraRtcEngine_iOS/4.1.0/AgoraVideoQualityAnalyzerExtension.xcframework.zip",
+            checksum: "8462bb37c1b55eec9bbdbaef01fb3492969a09b87154cfd3ceb798bfc6b60bd9"
         ),
         .target(
             name: "ChannelizeLSCSDK",
             dependencies: [
                 .target(name: "LiveBroadcastAPI"),
                 .target(name: "LiveBroadcastUI"),
+                .target(name: "AgoraAiEchoCancellationExtension"),
+                .target(name: "AgoraDav1d"),
+                .target(name: "AgoraDrmLoaderExtension"),
+                .target(name: "Agorafdkaac"),
+                .target(name: "Agoraffmpeg"),
+                .target(name: "AgoraRtcKit"),
+                .target(name: "AgoraSoundTouch"),
+                .target(name: "AgoraVideoQualityAnalyzerExtension"),
                 .product(name: "AWSIoT", package: "AWSiOSSDKV2"),
                 .product(name: "AWSCore", package: "AWSiOSSDKV2"),
-                .product(name: "RtcBasic", package: "AgoraRtcKit"),
-                .product(name: "VQA", package: "AgoraRtcKit"),
-                .product(name: "ClearVision", package: "AgoraRtcKit"),
-                .product(name: "AIAEC", package: "AgoraRtcKit"),
-                .product(name: "DRM", package: "AgoraRtcKit"),
-                .product(name: "Kingfisher", package: "Kingfisher"),
             ],
             path: "Framework/Dependency",
             exclude: []
